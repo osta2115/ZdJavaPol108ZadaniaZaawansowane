1eef19e098e8214be5da6005a442ddab377726a4
Uzytkownik <uzytkownik@kursGIT.pl>
2022-05-07 19:34:58 +0200

    task5 solution

---
+++ src/main/java/advance/exercises/day1/task5/SDAHashSet.java
@@ -0,0 +1,28 @@
+package advance.exercises.day1.task5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SDAHashSet<T> {
+    private final Map<T, ?> map = new HashMap<>();
+
+    public void add(T object){
+        map.put(object, null);
+    }
+
+    public void remove(T object) {
+        map.remove(object);
+    }
+
+    public int size() {
+        return map.size();
+    }
+
+    public boolean contains(T object) {
+        return map.containsKey(object);
+    }
+
+    public void clear(){
+        map.clear();
+    }
+}
---
+++ src/test/java/advance/exercises/day1/task5/SDAHashSetTest.java
@@ -0,0 +1,74 @@
+package advance.exercises.day1.task5;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SDAHashSetTest {
+
+    SDAHashSet<String> SDASet = new SDAHashSet<>();
+
+    @BeforeEach
+    void init(){
+        SDASet.add("sda");
+        SDASet.add("SDA");
+        SDASet.add("Sda");
+    }
+
+    @Test
+    void shouldAddValueToSet(){
+        //given
+        //when
+        SDASet.add("sdA");
+        //then
+        assertThat(SDASet.size()).isEqualTo(4);
+    }
+
+    @Test
+    void removeRemoveValueFromSet(){
+        //given
+        //when
+        SDASet.remove("Sda");
+        //then
+        assertThat(SDASet.size()).isEqualTo(2);
+    }
+
+    @Test
+    void shouldReturnSetSize(){
+        //given
+        //when
+        int result = SDASet.size();
+        //then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void shouldReturnTrueIfSetContainsValue() {
+        //given
+        //when
+        boolean result = SDASet.contains("SDA");
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void shouldReturnFalseIfSetDontContainsValue() {
+        //given
+        //when
+        boolean result = SDASet.contains("sdA");
+        //then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void shouldClearSet() {
+        //given
+        //when
+        SDASet.clear();
+        //then
+        assertThat(SDASet.size()).isEqualTo(0);
+    }
+
+}
