c7997938410f8fdf6db42ab29c72e59df67f6c68
Uzytkownik <uzytkownik@kursGIT.pl>
2022-05-14 16:16:13 +0200

    day2_task18_19_homework_solution

---
+++ src/main/java/advance/exercises/day2/task18_19/Laptop.java
@@ -0,0 +1,42 @@
+package advance.exercises.day2.task18_19;
+
+import java.util.Objects;
+
+public class Laptop extends Computer {
+
+    private String battery;
+
+    public Laptop(String procesor, int ram, String graphic, String producent, String model, String battery) {
+        super(procesor, ram, graphic, producent, model);
+        this.battery = battery;
+    }
+
+    public String getBattery() {
+        return battery;
+    }
+
+    public void setBattery(String battery) {
+        this.battery = battery;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "battery='" + battery + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Laptop laptop = (Laptop) o;
+        return Objects.equals(battery, laptop.battery);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), battery);
+    }
+}
