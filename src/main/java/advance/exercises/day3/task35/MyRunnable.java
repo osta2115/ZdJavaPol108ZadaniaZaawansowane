044775bfe1c9905b42d047afa41bea04b8993d2c
Uzytkownik <uzytkownik@kursGIT.pl>
2022-05-15 14:35:11 +0200

    day3_task35_solution

---
+++ src/main/java/advance/exercises/day3/task35/MyRunnable.java
@@ -0,0 +1,25 @@
+package advance.exercises.day3.task35;
+
+import java.util.Random;
+
+public class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.printf("%s - Rozpoczynam pracę \n", name);
+        System.out.println();
+        while (!Thread.interrupted()){
+            System.out.printf("%s - Pracuję \n", name);
+            System.out.printf("%s - Idę spać \n", name);
+//            try {
+//                Thread.sleep(1000 + new Random().nextInt(5000));
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            System.out.printf("%s - Wstałem i pracuję \n", name);
+            System.out.println();
+        }
+        System.out.printf("%s - Kończę pracę \n", name);
+    }
+}
---
+++ src/main/java/advance/exercises/day3/task35/Task35.java
@@ -0,0 +1,20 @@
+package advance.exercises.day3.task35;
+
+import java.util.concurrent.TimeUnit;
+
+public class Task35 {
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new MyRunnable());
+        Thread thread2 = new Thread(new MyRunnable());
+        thread1.start();
+        thread2.start();
+        try {
+            TimeUnit.SECONDS.sleep(2);
+            thread1.interrupt();
+            thread1.join();
+        } catch (InterruptedException e) {
+            System.out.println("Wątek został przerwany");
+        }
+    }
+}
